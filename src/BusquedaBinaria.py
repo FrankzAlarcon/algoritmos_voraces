4bd5195bd1a69a90a98b23d13e22824337ba8b95
Christian <patopazchris@gmail.com>
2022-02-06 23:10:07 -0500

    BinarioFinalComplej

--- src/BusquedaBinaria.py
+++ src/BusquedaBinaria.py
@@ -1,5 +1,6 @@
 import random
-#Metodo para ingresar elementos a una lista
+
+#Metodo para ingresar elementos a una lista manualmente
 def ingresarLista():
     lista=[]
     num=int(input("Cuantos elementos desea ingresar: "))
@@ -9,20 +10,27 @@ def ingresarLista():
     print(lista)
     lista.sort()
     return lista
-
+#metodo para ingresar una lista de elementos Random
 def ingresarListaRandom():
     lista = []
     num = int(input("Cuantos elementos desea ingresar: "))
     for a in range(1, num+1):
-        aux = random.randint(1,1000)
+        aux = random.randint(1,10000)
         lista.append(aux)  # Ingresamos el elemento a la lista
     print(lista)
     lista.sort()
     return lista
 
+
 #Metodo de busqueda Binaria implementando la tecnica de programación Divide y Venceras
 #Busca en toda la lista dividiéndola en segmentos 
+
+#La complejidad del caso peor es O(log2n). Cada iteración requiere una operación de comparación:
+#Total comparaciones ≈ 1 + log2n
+complejidad =0 
 def busquedaBinariaRecursiva(lista, bajo, alto, numero):
+    global complejidad
+    complejidad = complejidad + 1
     #Recibe una lista de elementos donde va a buscar determinado numero 
     #Bajo y Alto equivalen a los valores de las posiciones extremas del rango de la lista
     # Determina la posicion mitad de la lista  
@@ -36,25 +44,30 @@ def busquedaBinariaRecursiva(lista, bajo, alto, numero):
     elif lista[mitad] < numero: 
         #Si el numero a buscar es mayor que el elemento mitad de la lista, sigue buscando
         #se conserva el rango desde la mitad hacia la derecha [medio+1, derecha], descartando la otra mitad izquierda
+        
         return busquedaBinariaRecursiva(lista, mitad+1, alto, numero)
     else:
         #Caso Contrario, Si el numero a buscar es menor que el elemento mitad de la lista, sigue buscando
         #se conserva el rango desde la mitad hacia la izquierda [izquierda, medio-1] descartando la otra mitad derecha
-
-        return busquedaBinariaRecursiva(lista, bajo, mitad-1, numero)
     
+        return busquedaBinariaRecursiva(lista, bajo, mitad-1, numero)
+
+
+
 def menu():
     print('''
     \t*** Menu *** \t 
     1. Ingresar nueva lista
     2. Ingresar Lista Random
     3. Busqueda Binaria
+    ***  Total comparaciones ≈ 1 + O(log2n) **
     4. Salir
     ''')
     des=int(input("Seleccione una opcion: "))
     return des
 
 def main():
+    
     des=0
     listaAux = []
     while(des!=4):
@@ -67,14 +80,17 @@ def main():
             listaAux = ingresarListaRandom()
         #Opcion 3
         elif des==3:
+            global complejidad
+            complejidad =0
             num=int(input('Por favor ingrese el elemento a buscar: '))
             auxiliar = busquedaBinariaRecursiva(listaAux, 0, len(listaAux)-1, num)
-            print(auxiliar)
             print(listaAux)
+            
             if(auxiliar == -1):
                 print("No se encontro el elemento en la lista")
             else:
                 print(f'El elemento {num} se encuentra en la posición {auxiliar+1}')
+                print('Tiene una complejidad total de: ' + str(complejidad))
         #Opcion 4
         elif des==4:
             print('Saliendo....')
