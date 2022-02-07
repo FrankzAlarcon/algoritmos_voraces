63055e01910384858216c1e9e6f45bb0d670bcbf
Christian <patopazchris@gmail.com>
2022-02-06 22:24:12 -0500

    binariav2

--- src/BusquedaBinaria.py
+++ src/BusquedaBinaria.py
@@ -7,6 +7,7 @@ def ingresarLista():
         aux=input(f'Ingrese el elemento {a} : ')
         lista.append(aux) #Ingresamos el elemento a la lista
     print(lista)
+    lista.sort()
     return lista
 
 def ingresarListaRandom():
@@ -16,37 +17,36 @@ def ingresarListaRandom():
         aux = random.randint(1,1000)
         lista.append(aux)  # Ingresamos el elemento a la lista
     print(lista)
+    lista.sort()
     return lista
 
 #Metodo de busqueda Binaria implementando la tecnica de programación Divide y Venceras
 #Busca en toda la lista dividiéndola en segmentos 
-def busquedaBinaria(lista, numero):
-    #Recibe una lista de elementos donde iterar y el elemento a buscar
-    # Determina el rango del segmento a bucar que empieza en 0 y termina en len(lista) - 1.
-    inicio = 0  
-    final = len(lista)-1
-    lista.sort()  # Ordenamos la lista utilizando la funcion sort
-    #Itera mientras el rango del segmento a buscar tenga elementos
-    while inicio <= final:
-        #Seleccionamos la posicion mitad
-        mitad = int((inicio+final)/2)
-        #Verificamos si el elemento mitad es igual al numero a buscar
-        if lista[mitad]==int(numero):
-            #Se retorna su posicion
-            return mitad
-        #Si el numero a buscar es menor que el elemento mitad de la lista, sigue buscando
-        elif lista[mitad] > int(numero):
-            #se conserva el rango de la izquierda: [izq, medio-1], descartando la otra mitad de la derecha
-            final = mitad-1
+def busquedaBinariaRecursiva(lista, bajo, alto, numero):
+    #Recibe una lista de elementos donde va a buscar determinado numero 
+    #Bajo y Alto equivalen a los valores de las posiciones extremas del rango de la lista
+    # Determina la posicion mitad de la lista  
+    if bajo > alto: #Caso Base
+        print('base')
+        #Si el numero a buscar no se encontró en la lista
+        return -1
+    mitad = (int(bajo) + int(alto)) // 2 #Calculamos la poscion media de la lista
+    if lista[mitad] == numero:
+        print('bingo')
+        print(mitad)
+        #Si se ecnontró el numero a buscar en la lista
+        return mitad
+    elif lista[mitad] < numero: 
         #Si el numero a buscar es mayor que el elemento mitad de la lista, sigue buscando
-        else:
-            #se conserva el rango de la derecha: [izq, medio-1], descartando la otra mitad de la izquierda
-            inicio = mitad+1
-        # si no salió del ciclo, vuelve a iterar con el nuevo rango definico
-
-    # Si salió del ciclo sin retorn nada, el valor no se encuentra en la lista retornando -1
-    return -1
-
+        #se conserva el rango desde la mitad hacia la derecha [medio+1, derecha], descartando la otra mitad izquierda
+        print('mayor')
+        busquedaBinariaRecursiva(lista, mitad+1, alto, numero)
+    else:
+        #Caso Contrario, Si el numero a buscar es menor que el elemento mitad de la lista, sigue buscando
+        #se conserva el rango desde la mitad hacia la izquierda [izquierda, medio-1] descartando la otra mitad derecha
+        print('menor')
+        busquedaBinariaRecursiva(lista, bajo, mitad-1, numero)
+    
 def menu():
     print('''
     \t*** Menu *** \t 
@@ -71,10 +71,11 @@ def main():
             listaAux = ingresarListaRandom()
         #Opcion 3
         elif des==3:
-            num=input('Por favor ingrese el elemento a buscar: ')
-            auxiliar=int(busquedaBinaria(listaAux, num))
+            num=int(input('Por favor ingrese el elemento a buscar: '))
+            auxiliar = busquedaBinariaRecursiva(listaAux, 0, len(listaAux)-1, num)
+            print(auxiliar)
             print(listaAux)
-            if(auxiliar==-1):
+            if(auxiliar == -1):
                 print("No se encontro el elemento en la lista")
             else:
                 print(f'El elemento {num} se encuentra en la posición {auxiliar+1}')
