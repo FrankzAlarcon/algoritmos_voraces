7874e0e9c73af329a71b78d30d5513aec7e13e73
JhonattanStevenAmaguaTaco <jhonattan.amagua@epn.edu.ec>
2022-02-07 05:40:55 -0500

    Calculo de complejidad de Entrega cambio y optimizacion de Entrega cambio en caso de un array desordenado

--- src/EntregaCambio.py
+++ src/EntregaCambio.py
@@ -1,4 +1,5 @@
-# Definicion de la funcion es_solucion, ingresando como parametros un arraryque contiene el dato y la cantidad, y el valor a devolver
+from cmath import log
+# Definicion de la funcion es_solucion, ingresando como parametros un arrary que contiene el dato y la cantidad, y el valor a devolver
 def es_solucion(solucion, valor_devolver):
     #Inicializacion de total en 0
     total = 0
@@ -16,9 +17,12 @@ def es_solucion(solucion, valor_devolver):
  #Define la funcion que imprime un vector que almacena vectores donde se imprime el valor de la moneda y la cantidad de onedas
 def cambio(conjunto_candidatos, valor_devolver):
     solucion = []
+    #ordenamos el vector con la finalidad que en un inicio el algoritmo escoga las monedas de denominacion mas altas para que se use 
+    #La menor cantidad de monedas posible
+    conjunto_candidatos.sort()
 #inicializa el restante igualando con el valor_devolver ingresado
     restante = valor_devolver
-#Realiza repeticiones mientras el vector conjunto_candidatos tenga datos, y es_solucion sea falso
+#Realiza repeticiones mientras el vector conjunto_candidatos tenga datos, y es_solucion con los valores hasta ese entonces del vector solucion sea falso 
     while conjunto_candidatos and not es_solucion(solucion, valor_devolver):
         #Saca datos del vector conjunto_candidatos
         dato = conjunto_candidatos.pop()
@@ -32,10 +36,13 @@ def cambio(conjunto_candidatos, valor_devolver):
             restante = round(restante - (dato * cantidad), 2)
     #Si es_solucion da el valor de verdadero implica que existe una forma de devolver el valor, caso contrario  imprimie none        
     if(es_solucion(solucion, valor_devolver)):
+        #Calculo de complejidad
+        complejidad=(2*len(solucion) + 1 + len(solucion)*log(len(solucion)))
+        print("El valor de la complejidad del algoritmo es: ", complejidad)
         return solucion
     else:
         return None
 
-monedas = [0.01, 0.05, 0.1, 0.25, 0.5, 1]
-vuelto = cambio(monedas, 2.5)
-print(vuelto)
+monedas = [0.01, 0.05, 1, 0.25, 0.5, 0.1]
+vuelto = cambio(monedas, 0.75)
+print("La cantidad necesaria para devolver el vuelto es: ", vuelto)
